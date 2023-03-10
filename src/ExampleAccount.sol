e31e692bedd6b4774a9ce53f009119cc27e4596d
adam <adam@alchemy.com>
2023-03-10 08:22:19 -0800

    ExampleAccount

---
+++ src/ExampleAccount.sol
@@ -0,0 +1,199 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.12;
+
+/* solhint-disable avoid-low-level-calls */
+/* solhint-disable no-inline-assembly */
+/* solhint-disable reason-string */
+
+import "@eth-infinitism/interfaces/IAccount.sol";
+import "@eth-infinitism/interfaces/IEntryPoint.sol";
+import "@eth-infinitism/core/Helpers.sol";
+import "@openzeppelin/utils/cryptography/ECDSA.sol";
+import "@openzeppelin/proxy/utils/Initializable.sol";
+import "@openzeppelin/proxy/utils/UUPSUpgradeable.sol";
+
+contract ExampleAccount is IAccount, UUPSUpgradeable, Initializable {
+    using UserOperationLib for UserOperation;
+    using ECDSA for bytes32;
+
+    //return value in case of signature failure, with no time-range.
+    // equivalent to _packValidationData(true,0,0);
+    uint256 constant internal SIG_VALIDATION_FAILED = 1;
+
+    //filler member, to push the nonce and owner to the same slot
+    // the "Initializeble" class takes 2 bytes in the first slot
+    bytes28 private _filler;
+
+    //explicit sizes of nonce, to fit a single storage cell with "owner"
+    uint96 private _nonce;
+    address public owner;
+
+    IEntryPoint private immutable _entryPoint;
+
+    event ExampleAccountInitialized(IEntryPoint indexed entryPoint, address indexed owner);
+
+    /**
+     * return the account nonce.
+     * subclass should return a nonce value that is used both by _validateAndUpdateNonce, and by the external provider (to read the current nonce)
+     */
+    function nonce() public view virtual returns (uint256) {
+        return _nonce;
+    }
+
+    /**
+     * return the entryPoint used by this account.
+     * subclass should return the current entryPoint used by this account.
+     */
+    function entryPoint() public view virtual returns (IEntryPoint) {
+        return _entryPoint;
+    }
+
+    function validateUserOp(UserOperation calldata userOp, bytes32, uint256 missingAccountFunds)
+    external override virtual returns (uint256 validationData) {
+        _requireFromEntryPoint();
+        
+        bytes32 hash = keccak256(
+            abi.encode(
+                userOp.sender,
+                userOp.nonce,
+                userOp.initCode,
+                userOp.callData,
+                userOp.callGasLimit,
+                userOp.verificationGasLimit,
+                userOp.preVerificationGas,
+                userOp.maxFeePerGas,
+                userOp.maxPriorityFeePerGas,
+                userOp.paymasterAndData,
+                address(_entryPoint),
+                block.chainid
+            )
+        ).toEthSignedMessageHash();
+
+        if (owner != hash.recover(userOp.signature)) {
+            return SIG_VALIDATION_FAILED;
+        }
+
+        _validateAndUpdateNonce(userOp);
+
+        _payPrefund(missingAccountFunds);
+
+        validationData = 0;
+    }
+
+    /**
+     * ensure the request comes from the known entrypoint.
+     */
+    function _requireFromEntryPoint() internal virtual view {
+        require(msg.sender == address(entryPoint()), "account: not from EntryPoint");
+    }
+
+    /**
+     * validate the current nonce matches the UserOperation nonce.
+     * then it should update the account's state to prevent replay of this UserOperation.
+     * called only if initCode is empty (since "nonce" field is used as "salt" on account creation)
+     * @param userOp the op to validate.
+     */
+    function _validateAndUpdateNonce(UserOperation calldata userOp) internal virtual {
+        require(_nonce++ == userOp.nonce, "account: invalid nonce");
+    }
+
+    /**
+     * sends to the entrypoint (msg.sender) the missing funds for this transaction.
+     * subclass MAY override this method for better funds management
+     * (e.g. send to the entryPoint more than the minimum required, so that in future transactions
+     * it will not be required to send again)
+     * @param missingAccountFunds the minimum value this method should send the entrypoint.
+     *  this value MAY be zero, in case there is enough deposit, or the userOp has a paymaster.
+     */
+    function _payPrefund(uint256 missingAccountFunds) internal virtual {
+        if (missingAccountFunds != 0) {
+            (bool success,) = payable(msg.sender).call{value : missingAccountFunds, gas : type(uint256).max}("");
+            (success);
+            //ignore failure (its EntryPoint's job to verify, not account.)
+        }
+    }
+
+    modifier onlyOwner() {
+        _onlyOwner();
+        _;
+    }
+
+    // solhint-disable-next-line no-empty-blocks
+    receive() external payable {}
+
+    constructor(IEntryPoint anEntryPoint) {
+        _entryPoint = anEntryPoint;
+        _disableInitializers();
+    }
+
+    function _onlyOwner() internal view {
+        //directly from EOA owner, or through the account itself (which gets redirected through execute())
+        require(msg.sender == owner || msg.sender == address(this), "only owner");
+    }
+
+    /**
+     * execute a transaction (called directly from owner, or by entryPoint)
+     */
+    function execute(address dest, uint256 value, bytes calldata func) external {
+        _requireFromEntryPointOrOwner();
+        _call(dest, value, func);
+    }
+
+    /**
+     * execute a sequence of transactions
+     */
+    function executeBatch(address[] calldata dest, bytes[] calldata func) external {
+        _requireFromEntryPointOrOwner();
+        require(dest.length == func.length, "wrong array lengths");
+        for (uint256 i = 0; i < dest.length; i++) {
+            _call(dest[i], 0, func[i]);
+        }
+    }
+
+    function initialize(address anOwner) public virtual initializer {
+        owner = anOwner;
+        emit ExampleAccountInitialized(_entryPoint, owner);
+    }
+
+    // Require the function call went through EntryPoint or owner
+    function _requireFromEntryPointOrOwner() internal view {
+        require(msg.sender == address(entryPoint()) || msg.sender == owner, "account: not Owner or EntryPoint");
+    }
+
+    function _call(address target, uint256 value, bytes memory data) internal {
+        (bool success, bytes memory result) = target.call{value : value}(data);
+        if (!success) {
+            assembly {
+                revert(add(result, 32), mload(result))
+            }
+        }
+    }
+
+    /**
+     * check current account deposit in the entryPoint
+     */
+    function getDeposit() public view returns (uint256) {
+        return _entryPoint.balanceOf(address(this));
+    }
+
+    /**
+     * deposit more funds for this account in the entryPoint
+     */
+    function addDeposit() public payable {
+        _entryPoint.depositTo{value : msg.value}(address(this));
+    }
+
+    /**
+     * withdraw value from the account's deposit
+     * @param withdrawAddress target to send to
+     * @param amount to withdraw
+     */
+    function withdrawDepositTo(address payable withdrawAddress, uint256 amount) public onlyOwner {
+        _entryPoint.withdrawTo(withdrawAddress, amount);
+    }
+
+    function _authorizeUpgrade(address newImplementation) internal view override {
+        (newImplementation);
+        _onlyOwner();
+    }
+}
